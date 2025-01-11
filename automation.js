fd1a39a5b64c53b89e8721b262a571b6978ecdc9
Laila Sumiya Khan <nilokhan207@gmail.com>
2025-01-11 12:58:45 +0600

    completed the automation till checkout

--- automation.js
+++ automation.js
@@ -1,15 +1,71 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const driver = new Builder().forBrowser('chrome').build();
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const driver = new Builder().forBrowser("chrome").build();
 
-async function execution(){
-   await driver.manage().window().maximize();
-   await driver.get("https://demo.evershop.io/")
-   await driver.findElement(By.className("search-icon")).click();
-   await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Nike react phantom run flyknit 2");
-   await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(Key.RETURN);
-  const price = await driver.findElement(By.xpath("//span[contains(@class,'sale-price')]")).getText();
-  console.log(price);
-  await driver.sleep(5000);
-   await driver.quit();
+async function execution() {
+   try {
+      await driver.manage().window().maximize();
+      await driver.get("https://demo.evershop.io/");
+      // console.log("opened website");
+      await driver.findElement(By.className("search-icon")).click();
+      // console.log("clicked search icon");
+      await driver
+         .findElement(By.xpath("//input[@placeholder='Search']"))
+         .sendKeys("Nike react phantom");
+      // console.log("searched for Nike react phantom");
+      await driver
+         .findElement(By.xpath("//input[@placeholder='Search']"))
+         .sendKeys(Key.RETURN);
+      // console.log("pressed enter");
+      // const price = await driver
+      //    .findElement(By.xpath("//span[contains(@class,'sale-price')]"))
+      //    .getText();
+      const price = await driver
+         .findElement(
+            By.xpath(
+               "//div[contains(. , 'Nike react phantom run flyknit 2')]/following-sibling::div/div/span"
+            )
+         )
+         .getText();
+      console.log("price is: ", price);
+      await driver
+         .findElement(
+            By.xpath(
+               "//span[contains(text(),'Nike react phantom run flyknit 2')]"
+            )
+         )
+         .click();
+      // console.log("clicked on product");
+      await driver.findElement(By.xpath("//a[text()='X']")).click();
+      await driver.sleep(3000);
+      console.log("selected size");
+      await driver.findElement(By.xpath("//a[text()='Black']")).click();
+      // console.log("selected color");
+      await driver.findElement(By.xpath("//input[@name='qty']")).clear();
+      // console.log("cleared quantity");
+
+      await driver.findElement(By.xpath("//input[@name='qty']")).sendKeys("3");
+      // console.log("added quantity");
+      const button = await driver.findElement(
+         By.xpath(
+            "//button[contains(@class, 'primary') and span[text()='ADD TO CART']]"
+         )
+      );
+      await button.click();
+      // console.log("clicked add to cart button");
+      await driver.sleep(3000);
+
+      await driver
+         .findElement(By.xpath("//a[contains(text(), 'VIEW CART (1)')]"))
+         .click();
+      await driver.sleep(3000);
+      await driver
+         .findElement(By.xpath("//span[contains(text(), 'CHECKOUT')]"))
+         .click();
+      await driver.sleep(3000);
+      await driver.quit();
+      // console.log("closed browser");
+   } catch (error) {
+      console.error(error);
+   }
 }
 execution();
